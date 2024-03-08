512defb9a17e76c5716c7d152d52645c6e23ea04
TheRoshangar <162712330+TheRoshangar@users.noreply.github.com>
2024-03-08 20:33:43 +0330

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public abstract class Course {
+
+protected String CodeNumber;
+protected int CourseCredit;
+
+
+
+
+
+
+
+
+
+    public void removeLessonG(String course,int number){
+
+    }
+    public void removeLessonP(String course,int number){
+
+    }
+
+public void EE(String course){
+
+}
+    public void CE(String course){
+
+    }
+    public void ME(String course){
+
+    }
+    public void MATH(String course){
+
+    }
+
+    public void EEd(String course){
+
+    }
+    public void CEd(String course){
+
+    }
+    public void MEd(String course){
+
+    }
+    public void MATHd(String course){
+
+    }
+    public  int getCourseCredit() {
+        return CourseCredit;
+    }
+
+
+
+    public String getCodeNumber() {
+        return CodeNumber;
+    }
+
+
+
+    public void setCodeNumber(String codeNumber) {
+        CodeNumber = codeNumber;
+    }
+
+    public void setCourseCredit(int courseCredit) {
+        CourseCredit = courseCredit;
+    }
+
+
+
+}
